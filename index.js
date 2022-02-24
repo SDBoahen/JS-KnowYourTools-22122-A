89dda9238e170508a3b7c282f91286349d6083a5
SDBoahen <SDBoahen@gmail.com>
2022-02-24 16:41:07 -0500

    Worked on Const Variable Declaration  :)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Global 
+// Global 🌍💫
 
 
 
@@ -13,31 +13,84 @@
 
 
 
+
 // console . log( firstArgument , secondArgument )  //
 
-let a
-console.log( "This is the Value of  a  : " , a )  //
-a = 1
-console.log( "This is the Value of  a - NOW!  : " , a)  //
 
-let b
-console.log( "This is the Value of  b  : " , b )  //
-b = 2
-console.log( "This is the Value of  b - NOW!  : " , b)  //
+//// Variables - let 
 
+// let a
+// console.log( "This is the Value of  a  : " , a )  //
+// a = 1
+// console.log( "This is the Value of  a - NOW!  : " , a)  //
 
-
-
-let s = 3
-console.log(s)  //
-
-s = "Sam"
-console.log(s)  //
+// let b
+// console.log( "This is the Value of  b  : " , b )  //
+// b = 2
+// console.log( "This is the Value of  b - NOW!  : " , b)  //
 
 
-let amy = "Amy"
-console.log(amy)  //
 
+
+// let s = 3
+// console.log(s)  //
+
+// s = "Sam"
+// console.log(s)  //
+
+
+// let amy = "Amy"
+// console.log(amy)  //
+
+
+
+
+//// Variables - const 
+//  "const CAN'T CHANGE"
+
+
+// console . log( firstArgument , secondArgument )  //
+
+
+//// Assign Equals SIGN
+//// As(SIGN) Equals(SIGN)
+console.log("--------------------------------")  //
+
+let variableDeclaredWithLet = 4
+console.log("This is variableDeclaredWithLet: ", variableDeclaredWithLet)  //
+const variableDeclaredWithConst = 4
+console.log("This is variableDeclaredWithConst: ", variableDeclaredWithConst)  //
+
+console.log("----------------")  //
+
+variableDeclaredWithLet = 4
+console.log("This is variableDeclaredWithLet, NOW: ", variableDeclaredWithLet)  //
+// variableDeclaredWithConst = 4 ///// THIS IS A NONO  :(
+console.log("This is variableDeclaredWithConst, NOW: ", variableDeclaredWithConst)  //
+
+console.log("----------------")  //
+
+// let variableDeclaredWithLet = [] ///// THIS IS A NONO  :(
+// const variableDeclaredWithConst = [] ///// THIS IS A NONO  :(
+
+
+// string, integer, BigInt, boolean
+// Reference - Refer To
+
+const arrayDeclaredWithConstTHATSAMISGONNACHANGE = []
+console.log("This is variableDeclaredWithConstTHATSAMISGONNACHANGE: ", arrayDeclaredWithConstTHATSAMISGONNACHANGE)  //
+
+// arrayDeclaredWithConstTHATSAMISGONNACHANGE = [ "Alexis" ] ///// THIS IS A NONO  :(
+
+    //                                     []s .push( theThingYouWantToPushIntoTheArray ) ~ a Function
+    //                                         .push returns ????
+    //                                     []s .push END RESULT :  [ theThingYouWantToPushIntoTheArray ]
+    arrayDeclaredWithConstTHATSAMISGONNACHANGE.push( "Alexis" )
+console.log("This is variableDeclaredWithConstTHATSAMISGONNACHANGE: ", arrayDeclaredWithConstTHATSAMISGONNACHANGE)  //
+
+
+
+console.log("--------------------------------")  //
 
 
 
