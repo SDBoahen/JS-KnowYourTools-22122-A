fa145ce1f9cdedcf473802ad89b818cfb0823ceb
SDBoahen <SDBoahen@gmail.com>
2022-02-23 19:58:22 -0500

    Day-2: #Let'sStartFiguringThisOut <3

---
+++ index.js
@@ -0,0 +1,88 @@
+// Global 
+
+
+
+
+//// Variable 
+
+// Something that holds information 
+// We can reference that information dynamically 
+//// Vari able 
+// Something that is able - to VARY 
+
+
+
+
+// console . log( firstArgument , secondArgument )  //
+
+let a
+console.log( "This is the Value of  a  : " , a )  //
+a = 1
+console.log( "This is the Value of  a - NOW!  : " , a)  //
+
+let b
+console.log( "This is the Value of  b  : " , b )  //
+b = 2
+console.log( "This is the Value of  b - NOW!  : " , b)  //
+
+
+
+
+let s = 3
+console.log(s)  //
+
+s = "Sam"
+console.log(s)  //
+
+
+let amy = "Amy"
+console.log(amy)  //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("whateveeerrrr")  //
+
+
+
+
+
+
+function hey(){
+
+    console.log("hey! 👋🤪")
+
+}
+
+// hey()
+// invoking
+// calling
+// activating 
